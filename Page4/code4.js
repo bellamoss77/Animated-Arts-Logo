42bf2bc61254b04c0cd9d5c21c5b90f572083368
Maggie <mmf295@icloud.com>
2024-02-26 12:15:48 -0500

    add animation to ar

--- Page4/code4.js
+++ Page4/code4.js
@@ -2,6 +2,7 @@ const logoElements = document.querySelector('.logoElements');
 const pv = document.querySelector('.pv');
 const cc = document.querySelector('.cc');
 const ar = document.querySelector('.ar');
+console.log('ar element', ar);
 const ts = document.querySelector('.ts');
 
 let masterTl = gsap.timeline();
@@ -46,5 +47,31 @@ masterTl.to(cc, {
     rotation: 0,
     duration: 1,
     ease: 'elastic'
+})
+masterTl.fromTo(ar, {
+    scale: 0,
+    y: 300,
+    x: 300,
+    rotation: -360
+},
+{   scale: 1,
+    x: -50,
+    y: -50,
+    rotation: 0,
+    duration: 2,
+    ease: 'bounce',
+    onStart: () => console.log('Animation start for ar - fromTo'),
+    onUpdate: () => console.log('Animation update for ar - fromTo'),
+    onComplete: () => console.log('Animation complete for ar - fromTo')
+});
+masterTl.to(ar, {
+    y: 0,
+    x: 0,
+    rotation: 360,
+    duration: 1,
+    ease: 'elastic',
+    onStart: () => console.log('Animation start for ar - to'),
+    onUpdate: () => console.log('Animation update for ar - to'),
+    onComplete: () => console.log('Animation complete for ar - to')
 })
 
