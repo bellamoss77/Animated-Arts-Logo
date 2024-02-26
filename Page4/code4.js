afb3a69085bc6840867ecd5459a20c5bd82604de
Maggie <mmf295@icloud.com>
2024-02-26 15:29:18 -0500

    start on page 5

--- Page4/code4.js
+++ Page4/code4.js
@@ -2,8 +2,8 @@ const logoElements = document.querySelector('.logoElements');
 const pv = document.querySelector('.pv');
 const cc = document.querySelector('.cc');
 const ar = document.querySelector('.ar');
-console.log('ar element', ar);
 const ts = document.querySelector('.ts');
+const presents = document.querySelector('.presents')
 
 let masterTl = gsap.timeline();
 
@@ -55,23 +55,63 @@ masterTl.fromTo(ar, {
     rotation: -360
 },
 {   scale: 1,
-    x: -50,
-    y: -50,
+    x: -100,
+    y: -100,
     rotation: 0,
     duration: 2,
     ease: 'bounce',
-    onStart: () => console.log('Animation start for ar - fromTo'),
-    onUpdate: () => console.log('Animation update for ar - fromTo'),
-    onComplete: () => console.log('Animation complete for ar - fromTo')
-});
+}, '<');
 masterTl.to(ar, {
     y: 0,
     x: 0,
     rotation: 360,
     duration: 1,
     ease: 'elastic',
-    onStart: () => console.log('Animation start for ar - to'),
-    onUpdate: () => console.log('Animation update for ar - to'),
-    onComplete: () => console.log('Animation complete for ar - to')
+}, '-=0.5')
+masterTl.fromTo(ts, {
+    scale: 0,
+    y: -200,
+    x: 300,
+    rotation: -720
+},
+{
+    scale: 1,
+    y: 100,
+    x: -50,
+    rotation: 0,
+    duration: 2,
+    ease: 'bounce'
+}, '<')
+masterTl.to(ts, {
+    y: 0,
+    x: 0,
+    rotation: 360,
+    duration: 1,
+    ease: 'elastic'
 })
+masterTl.to(presents, {
+    rotationX: 0,
+    x: 100,
+    y: 100,
+    z: 0,
+    duration: 0.5,
+    ease: 'power2.inOut'
+}, '-=1');
+
+gsap.set('.presents path', { stroke: 'transparent' });
+
+document.querySelectorAll('.presents path').forEach((path, index) => {
+    const length = path.getTotalLength();
+    gsap.set(path, { strokeDasharray: length, strokeDashoffset: length });
+    masterTl.to(path, {
+        stroke: '#FFFFFF',
+        duration: 0.1,
+        immediateRender: false
+    })
+    masterTl.to(path, {
+        strokeDashoffset: 0,
+        duration: 0.25,
+        ease: 'power2.inOut'
+    });
+});
 
